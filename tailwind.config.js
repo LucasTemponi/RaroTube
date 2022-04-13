e0e1fd22c834ba44f20df9cbc49919a6ae5fbe30
Mauricio Silva <mauriciospta@gmail.com>
2022-04-13 14:06:03 -0300

    refazendo o create-react-app

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        raro: {
+          cobalto: '#343090',
+          oceano: '#4647C2',
+          violeta: '#7475D1',
+          maximum: '#B5B3E6',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
