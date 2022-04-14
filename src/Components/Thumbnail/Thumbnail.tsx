d334c2c63220e215f11a537e8a6023bf56906e38
LucasTemponi <lucastmpn@gmail.com>
2022-04-14 12:50:52 -0300

    Corrige bug de play/pause

--- src/Components/Thumbnail/Thumbnail.tsx
+++ src/Components/Thumbnail/Thumbnail.tsx
@@ -1,31 +1,33 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { thumbnailProps } from './ThumbnailProps';
 
 export const Thumbnails:React.FC<thumbnailProps> = (video) =>{
     const videoRef = useRef<HTMLVideoElement>(null);
-    const [playing,setPlaying] = useState<boolean>(false)
 
-    const onHover = () =>{
-        if(videoRef.current && !playing){ 
+    const playVideo = () =>{
+        if(videoRef.current){ 
             videoRef.current.play();
             videoRef.current.controls = true;
-            setPlaying(true)
             console.log(`${video.nome} is playing`)
-        }else if (videoRef.current && playing){
+        }
+    }
+
+    const pauseVideo = () =>{
+        if (videoRef.current){
             videoRef.current?.pause();
             videoRef.current.controls = false;
-            setPlaying(false);
             console.log(`${video.nome} is paused`)
-        }
+        }    
     }
 
     return(
         <div>
-            <video className = 'transform hover:scale-110 ease-linear duration-300 w-4/5'
+            <video className = 'transform hover:scale-110 ease-linear duration-300 w-4/5 m-0'
              title={video.nome}
              ref={videoRef}
-             onPointerLeave={onHover}
-             onPointerEnter={onHover}>
+             muted
+             onPointerLeave={pauseVideo}
+             onPointerEnter={playVideo}>
                  <source src={video.url}/>
             </video>
         </div>
