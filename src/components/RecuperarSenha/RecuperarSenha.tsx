313dfb40a31d8c0af82c8a5236f4fc92dc38a28a
Kamilla <laysa.viana@outlook.com>
2022-04-17 17:04:46 -0300

    Botão
    Botão com texto 'Alterar senha'(?) e função submit (não funcional)

--- src/components/RecuperarSenha/RecuperarSenha.tsx
+++ src/components/RecuperarSenha/RecuperarSenha.tsx
@@ -7,39 +7,47 @@ export const RecuperarSenha = () => {
   const [novaSenha, setNovaSenha] = useState("");
   const [confirmarSenha, setConfirmarSenha] = useState("");
 
+  const alterarSenha = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    //Código para alterar senha
+  }
+
   return (
     <>
-      <div>
-        <Input
-          name="codigo"
-          label="Código de Recuperação"
-          placeholder="Código de Recuperação"
-          type="text"
-          value={codigo}
-          onChange={(event) => { setCodigo(event.target.value) }}
-        />
-      </div>
+      <form action="#" onSubmit={alterarSenha}>
+        <div>
+          <Input
+            name="codigo"
+            label="Código de Recuperação"
+            placeholder="Código de Recuperação"
+            type="text"
+            value={codigo}
+            onChange={(event) => { setCodigo(event.target.value) }}
+          />
+        </div>
 
-      <div>
-        <Input
-          name="NovaSenha"
-          label="Nova Senha"
-          placeholder="Nova Senha"
-          type="text"
-          value={novaSenha}
-          onChange={(event) => { setNovaSenha(event.target.value) }}
-        />
-      </div>
+        <div>
+          <Input
+            name="NovaSenha"
+            label="Nova Senha"
+            placeholder="Nova Senha"
+            type="text"
+            value={novaSenha}
+            onChange={(event) => { setNovaSenha(event.target.value) }}
+          />
+        </div>
 
-      <div>
-        <Input name="confirmarSenha"
-          label="Confirmar Senha"
-          placeholder="Confirmar Senha"
-          type="text"
-          value={confirmarSenha}
-          onChange={(event) => { setConfirmarSenha(event.target.value) }}
-        />
-      </div>
+        <div>
+          <Input name="confirmarSenha"
+            label="Confirmar Senha"
+            placeholder="Confirmar Senha"
+            type="text"
+            value={confirmarSenha}
+            onChange={(event) => { setConfirmarSenha(event.target.value) }}
+          />
+        </div>
+        <Button type="submit">Alterar senha</Button>
+      </form>
     </>
   )
 
