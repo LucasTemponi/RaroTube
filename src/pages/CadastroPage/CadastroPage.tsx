89ace0aa012fe62faf75293569713806ab6d9ee2
Cássia Luísa dos Santos Pedrosa <cassiapedrosa141@gmail.com>
2022-04-20 21:40:39 -0300

    Implementa alert para caso de erro na requisição.

--- src/pages/CadastroPage/CadastroPage.tsx
+++ src/pages/CadastroPage/CadastroPage.tsx
@@ -6,26 +6,40 @@ import { UsuarioProps } from "../../components/Cadastro/UsuarioProps";
 export const CadastroPage = () => {
 
     const [usuario, setUsuario] = useState<UsuarioProps>();
+    //const navigate = useNavigate();
+
 
     const handleSubmit = async (usuario: UsuarioProps) => {
-        const response = await axios.post('https://3.221.159.196:3320/auth/cadastrar',
-            {
-                'nome': usuario.nome,
-                'email': usuario.email,
-                'senha': usuario.senha,
-                'codigo': usuario.turma.id
+
+        try {
+            const response = await axios.post('https://3.221.159.196:3320/auth/cadastrar',
+                {
+                    'nome': usuario.nome,
+                    'email': usuario.email,
+                    'senha': usuario.senha,
+                    'codigo': usuario.turma.id
+                }
+            );
+            setUsuario(response.data)
+            //navigate('/videos')
+
+        } catch (error: any) {
+            if (error.response.data.statusCode === 400) {
+                alert('Esse usuário já está cadastrado, siga para a página de Login.');
+            } else {
+                alert('Erro. Tente novamente mais tarde.');
             }
-        );
-        setUsuario(response.data)
+        }
     }
-    //navigate('/videos')
+
 
     return (
         <>
             <div className="items-center justify-center m-10">
                 <Cadastro
                     usuario={usuario}
-                    onSubmit={handleSubmit} />
+                    onSubmit={handleSubmit}
+                />
             </div>
         </>
     );
