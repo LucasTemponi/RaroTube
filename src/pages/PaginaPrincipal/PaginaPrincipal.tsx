7c1f595d26ab261e5d982894a41b9b085c7b757b
Cássia Luísa dos Santos Pedrosa <cassiapedrosa141@gmail.com>
2022-04-24 19:58:33 -0300

    Implementa renderização p/ usuários logados ou não logados

--- src/pages/PaginaPrincipal/PaginaPrincipal.tsx
+++ src/pages/PaginaPrincipal/PaginaPrincipal.tsx
@@ -2,25 +2,26 @@ import { useEffect, useMemo, useState } from "react"
 import Navbar from "../../components/Navbar/Navbar"
 import { VideoList } from "../../components/VideoList/VideoList"
 import { videoProps } from "../../components/VideoPlayer/VideoProps"
+import { useAuthContext } from "../../context/authContext"
 import { useFavoritos } from "../../hooks/useFavoritos"
 import apiClient from "../../services/api-client";
 
 export const PaginaPrincipal = () => {
 
-    const [ videos, setVideos ] = useState<videoProps[]>();
-    const [ recomendados, setRecomendados ] = useState<videoProps[]>();
-    const [ carregando, setCarregando ] = useState<boolean>(true);
-    const [ logado, setLogado ] = useState<boolean>(true);
+    const [videos, setVideos] = useState<videoProps[]>();
+    const [recomendados, setRecomendados] = useState<videoProps[]>();
+    const [carregando, setCarregando] = useState<boolean>(true);
+    const authContext = useAuthContext();
 
-    const iniciaFavoritos = useFavoritos(state=>state.iniciaFavoritos);
-    const todosFavoritos = useFavoritos(state=>state.favoritos);
+    const iniciaFavoritos = useFavoritos(state => state.iniciaFavoritos);
+    const todosFavoritos = useFavoritos(state => state.favoritos);
 
-    const loadVideos = async () =>{
+    const loadVideos = async () => {
         await Promise.all([
             apiClient.get('/videos').then(response => setVideos(response.data)),
             apiClient.get('/videos/favoritos').then(response => iniciaFavoritos(response.data)),
             //axiosapiInstance.get('https://3.221.159.196:3320/videos/')
-            ])
+        ])
     }
 
     // const videosFavoritos = useMemo(()=>{
@@ -37,28 +38,41 @@ export const PaginaPrincipal = () => {
     //     return temp;
     // },[todosFavoritos])
 
-    useEffect(()=>{
+    useEffect(() => {
         loadVideos()
         setCarregando(false)
-    },[])
+    }, [])
 
-    return(
-        <>
-            <Navbar />
-            {carregando ? <div>Carregando...</div> :
-            <div className="4xl:max-w-[70vw] xl:max-w-[80vw] lg:w-[85vw] 
+    if (authContext.estaAutenticado()) {
+        return (
+            <>
+                <Navbar />
+                {carregando ? <div>Carregando...</div> : <></>}
+                <div className="4xl:max-w-[70vw] xl:max-w-[80vw] lg:w-[85vw] 
                 md:w-[90vw] sm:w-[95vw] m-auto" >
-                {logado ? 
-                    <>
-                        <h1 className=" font-extrabold underline decoration-raro-rosa text-4xl m-4 mt-12 text-left " >Vídeos favoritos</h1>
-                        <VideoList videos={todosFavoritos} />
-                    </> :
-                    ''
-                }
-                <h1 className=" font-extrabold underline decoration-raro-oceano text-4xl m-4 mt-12 text-left">Adicionados recentemente</h1>
-                <VideoList videos={videos} />
-                <h1 className=" font-extrabold underline decoration-raro-violeta text-4xl m-4 mt-12 text-left">Recomendados</h1>
-            </div>}
-        </>
-    )
+                </div>
+                <>
+                    <h1 className=" font-extrabold underline decoration-raro-rosa text-4xl m-4 mt-12 text-left " >Vídeos favoritos</h1>
+                    <VideoList videos={todosFavoritos} />
+                    <h1 className=" font-extrabold underline decoration-raro-oceano text-4xl m-4 mt-12 text-left">Adicionados recentemente</h1>
+                    <VideoList videos={videos} />
+                    <h1 className=" font-extrabold underline decoration-raro-violeta text-4xl m-4 mt-12 text-left">Recomendados</h1>
+                </>
+            </>
+        )
+    }
+    else {
+        return (
+            <>
+                <Navbar />
+                {carregando ? <div>Carregando...</div> : <></>}
+                <div className="4xl:max-w-[70vw] xl:max-w-[80vw] lg:w-[85vw] 
+                md:w-[90vw] sm:w-[95vw] m-auto" >
+                    <h1 className=" font-extrabold underline decoration-raro-oceano text-4xl m-4 mt-12 text-left">Adicionados recentemente</h1>
+                    <VideoList videos={videos} />
+                    <h1 className=" font-extrabold underline decoration-raro-violeta text-4xl m-4 mt-12 text-left">Recomendados</h1>
+                </div>
+            </>
+        )
+    }
 }
