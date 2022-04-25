6db239454e80261fc319ec1d7063db91f76a003c
Mauricio Silva <mauriciospta@gmail.com>
2022-04-25 10:18:06 -0300

    remove css do arquivo

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,32 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import AdicionarComentario from './components/AdicionarComentario/AdicionarComentario';
-import { Cadastro } from './components/Cadastro/Cadastro';
 import { CodigoDeRecuperarSenha } from './components/CodigoDeRecuperarSenha/CodigoDeRecuperarSenha';
-import Comentario from './components/Comentario/Comentario';
 import { Layout } from './components/Layout/Layout';
-import ListaComentarios from './components/ListaComentarios/ListaComentarios';
-import { Login } from './components/Login/Login';
 import { RecuperarSenha } from './components/RecuperarSenha/RecuperarSenha';
 import { RequireAuth } from './components/RequireAuth/RequireAuth';
-//import { SolicitarCodigo } from './components/SolicitarCodigo/SolicitarCodigo';
-import { VideoList } from './components/VideoList/VideoList';
 import { AuthContextProvider } from './context/authContext';
 import { CadastroPage } from './pages/CadastroPage/CadastroPage';
 import { LoginPage } from './pages/LoginPage/LoginPage';
 import { PaginaPrincipal } from './pages/PaginaPrincipal/PaginaPrincipal';
 import VideoPage from './pages/VideoPage/VideoPage';
 
-
 function App() {
   return (
-    <div className='App'>
-      <AuthContextProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />} />
-            <Route index element={<PaginaPrincipal />} />
-            <Route path="/video/:id" element={<VideoPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/cadastro" element={<CadastroPage />} />
-            <Route path="/solicitarcodigo" element={<CodigoDeRecuperarSenha />} />
-            <Route path="/alterarsenha" element={<RecuperarSenha />} />
-            <Route element={<RequireAuth />}>
-              {/* As rotas que precisam de autenticação vem aqui */}
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </AuthContextProvider>
-    </div>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />} />
+          <Route index element={<PaginaPrincipal />} />
+          <Route path='/video/:id' element={<VideoPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/cadastro' element={<CadastroPage />} />
+          <Route path='/solicitarcodigo' element={<CodigoDeRecuperarSenha />} />
+          <Route path='/alterarsenha' element={<RecuperarSenha />} />
+          <Route element={<RequireAuth />}>
+            {/* As rotas que precisam de autenticação vem aqui */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthContextProvider>
   );
 }
 
