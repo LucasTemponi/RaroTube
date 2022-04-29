8e5c332dd7b5fb17f7051cdc0f86c6df5d10e000
Cássia Luísa dos Santos Pedrosa <cassiapedrosa141@gmail.com>
2022-04-29 16:51:34 -0300

    identação página principal.

--- src/pages/PaginaPrincipal/PaginaPrincipal.tsx
+++ src/pages/PaginaPrincipal/PaginaPrincipal.tsx
@@ -15,8 +15,8 @@ export const PaginaPrincipal = () => {
     // const VideoList = lazy(()=>import('../../components/VideoList/VideoList'));
 
     // const [videos, setVideos] = useState<VideoProps[]>();
-    const videos = useVideos(state=>state.videos);
-    const iniciaVideos = useVideos(state=>state.iniciaVideos);
+    const videos = useVideos(state => state.videos);
+    const iniciaVideos = useVideos(state => state.iniciaVideos);
     const [recomendados, setRecomendados] = useState<videoProps[]>();
     const [carregando, setCarregando] = useState<boolean>(true);
     const authContext = useAuthContext();
@@ -28,17 +28,17 @@ export const PaginaPrincipal = () => {
 
     const loadVideos = async () => {
         setCarregando(true);
-        try{
+        try {
             if (authContext.estaAutenticado()) {
                 await Promise.all([
                     apiClient.get('/videos').then(response => iniciaVideos(response.data.reverse())),
                     apiClient.get('/videos/favoritos').then(response => iniciaFavoritos(response.data)),
-                    ]);
+                ]);
             } else {
-                await apiClient.get('/videos').then(response => iniciaVideos(response.data.reverse()));    
+                await apiClient.get('/videos').then(response => iniciaVideos(response.data.reverse()));
             }
             setCarregando(false);
-        }catch(e){
+        } catch (e) {
             console.log(e)
         }
     }
@@ -49,29 +49,29 @@ export const PaginaPrincipal = () => {
         } else {
             setCarregando(false);
         }
-        
+
         // setTimeout(()=>setCarregando(false),5000)
     }, [])
 
     return (
         carregando ? <LazyPrincipal /> :
-        <>
-            <div className="4xl:max-w-[70vw] xl:max-w-[80vw] lg:w-[85vw] md:w-[90vw] sm:w-[95vw] m-auto" >
-            </div>
+            <>
+                <div className="4xl:max-w-[70vw] xl:max-w-[80vw] lg:w-[85vw] md:w-[90vw] sm:w-[95vw] m-auto" >
+                </div>
                 {
                     authContext.estaAutenticado() &&
                     (<>
                         <h1 className=" font-extrabold underline decoration-raro-rosa text-4xl m-4 mt-12 text-left " >Vídeos favoritos</h1>
-                            <VideoList videos={todosFavoritos} />
-                        
+                        <VideoList videos={todosFavoritos} />
+
                     </>)
                 }
                 <h1 className=" font-extrabold underline decoration-raro-oceano text-4xl m-4 mt-12 text-left">Adicionados recentemente</h1>
-                    <VideoList videos={videos?.slice(0, 10)} />
+                <VideoList videos={videos?.slice(0, 10)} />
                 <h1 className=" font-extrabold underline decoration-raro-violeta text-4xl m-4 mt-12 text-left">Recomendados</h1>
-                    <VideoList videos={videos?.slice(0, pagina * 15)} />
+                <VideoList videos={videos?.slice(0, pagina * 15)} />
 
-                <div ref={containerRef} className="h-10" /> 
-        </>
+                <div ref={containerRef} className="h-10" />
+            </>
     )
 }
