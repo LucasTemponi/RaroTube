4b484fc08b2a371cb3f413f659e1b47c3cb9c3eb
Kamilla <97555228+LaysaViana@users.noreply.github.com>
2022-04-28 23:43:46 -0300

    Ajuste de tamanho

--- src/pages/PaginaNaoEncontrada/PaginaNaoEncontrada.tsx
+++ src/pages/PaginaNaoEncontrada/PaginaNaoEncontrada.tsx
@@ -5,8 +5,8 @@ export const PaginaNaoEncontrada = () => {
         <img className="object-none"
           src="/erro404.jpg"
         />
-        <h3 className="font-medium leading-tight text-sky-400 mt-2 mb-5 text-2xl font-bold">Ops...</h3>
-        <p className="leading-normal mt-0 mb-10">A gente procurou, mas não encontrou esta página </p>
+        <h3 className="font-medium leading-tight text-sky-400 mt-2 mb-5 text-4xl font-bold">Ops...</h3>
+        <p className="leading-normal mt-0 mb-10 text-lg">A gente procurou, mas não encontrou esta página </p>
         <a href="/" className="text-sky-400 border-solid border-sky-400 border p-3 mt-6 rounded-full hover:text-white hover:bg-sky-400 ">Volte ao Início</a>
       </div>
     </div>
