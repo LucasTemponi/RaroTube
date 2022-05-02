06843ac72d9ff79dd16917e5fdb3a9da3c38fc93
Mauricio Silva <mauriciospta@gmail.com>
2022-05-02 17:48:25 -0300

    corrige margem

--- src/components/FavoritosVazio/FavoritosVazio.tsx
+++ src/components/FavoritosVazio/FavoritosVazio.tsx
@@ -1,10 +1,10 @@
 export const FavoritosVazio = () => {
   return (
-
-    <p className='my-5 min-h-[10em] dark:text-gray-100 text-sm md:text-lg text-center text-raro-cobalto font-bold'>
-      Para adicionar um vídeo aos favoritos, basta clicar na estrela no canto
-      superior direito do vídeo.
-    </p>
+    <div className='flex justify-center min-h-[10em]'>
+      <p className='m-auto min-h-[10em] dark:text-gray-100 text-sm md:text-lg text-center text-raro-cobalto font-bold'>
+        Para adicionar um vídeo aos favoritos, basta clicar na estrela no canto
+        superior direito do vídeo.
+      </p>
+    </div>
   );
 };
-
